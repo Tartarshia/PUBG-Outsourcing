4e141a3d70d29b9c6175275cca5da6f8a08f9688
Tatasha <tartarshia@gmail.com>
2017-11-14 21:08:45 -0500

    Backup to hub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = '欢迎来到Titan 世界';
+  language : any;
+  checked: boolean = true;
+  Panel: string = 'func';
+
+  
+  constructor( ) {
+    
+      }
+
+  localization() {
+    // English Text not Avaliable
+  }
+
+  showFunc(input) {
+    this.Panel = input;
+  }
+
+  
+  ngOnInit() {
+
+    this.language = window.navigator.language.substr(0,2);
+    if(this.language == "zh") {
+      this.localization();
+    }
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ButtonModule, ToolbarModule, FieldsetModule, InputSwitchModule }  from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    ButtonModule,
+    ToolbarModule,
+    FieldsetModule,
+    InputSwitchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
